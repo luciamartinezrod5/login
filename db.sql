8dcc88c5c5dd76e7f553604f32b80ed296738ef5
luciamartinezrod5 <luciamartinezrod5@gmail.com>
2025-12-03 13:22:36 -0300

    add: Add bd

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS mi_app;
+USE mi_app;
+
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
+  password VARCHAR(255)
+);

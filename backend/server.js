2c8fa971da4f36f8d43ac9b55e840e85f5ff78bf
luciamartinezrod5 <luciamartinezrod5@gmail.com>
2025-12-04 19:59:05 -0300

    add: Add function requiereSession, function redirectIfLogged y Hash bcrypt

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@
 // npm install express
 // npm install mysql2
 // npm install express mysql2 cors
+// npm install bcrypt  >>>>>> para hash contraseñas
 
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const bcrypt = require("bcrypt")
 
 const app = express();
 app.use(express.json());
@@ -26,9 +28,11 @@ app.post("/registro", (req, res) => {
 
     const { nombre, email, password } = req.body; // recupera los datos que se enviaron en scriptRegistro.js
 
+    const passwordHash = bcrypt.hashSync(password, 10);
+
     const sql = "INSERT INTO usuarios (nombre, email, password) VALUES (?, ?, ?)"; // sentencia sql para agregar un usuario
 
-    db.query(sql, [nombre, email, password], (err, result) => {
+    db.query(sql, [nombre, email, passwordHash], (err, result) => {
         if (err) {
             return res.json({ ok: false, error: "Error en la BD: " + err }); // si hay un err (error) devuelve ok -> false y error -> Error en la bd..
         }
@@ -45,6 +49,8 @@ app.post("/login", (req, res) => {
 
     const { email, password } = req.body;
 
+    const passwordHash = bcrypt.hash(password, 10);
+
     const sql = "SELECT * FROM usuarios WHERE email = ?"; // sentencia para buscar usuarios con el email ingresado
 
     db.query(sql, [email], (err, results) => {
@@ -56,9 +62,12 @@ app.post("/login", (req, res) => {
 
         const usuario = results[0]; // si encuentra el email el primer elemento del array será un objeto usuario
 
-        if (usuario.password !== password) { // verifica si la contraseña del obj usuario encontrado es distinta a la ingresada
-            return res.json({ ok: false, error: "Contraseña incorrecta." }); // error de contraseña diferente
-        }
+       // Comparar contraseña ingresada (texto plano) con el hash guardado
+            const coincide = bcrypt.compareSync(password, usuario.password);
+            if (!coincide) {
+                return res.json({ ok: false, error: "Contraseña incorrecta." });
+            }
+
 
         return res.json({ ok: true, nombre: usuario.nombre }); // la contraseña coincide e inicia la sesion
     });
--- utils.js
+++ utils.js
@@ -2,3 +2,31 @@ function error(texto) {
     document.getElementById("error").innerHTML =
         `<div class="alert alert-danger">${texto}</div>`;
 }
+
+// SOLO PARA PAGINAS PRIVADAS
+function requireSession() {
+    const usuarioJSON = localStorage.getItem("usuario");
+
+    if (!usuarioJSON) {
+        window.location.href = "login.html"; // No hay sesión → redirigir
+    } else {
+        const usuario = JSON.parse(usuarioJSON);
+        document.getElementById("usuario").textContent = usuario.nombre;
+    }
+}
+
+
+// SOLO PARA LOGIN Y REGISTRO
+function redirectIfLogged() {
+    const usuarioJSON = localStorage.getItem("usuario");
+
+    if (usuarioJSON) {
+        window.location.href = "home.html"; // Ya hay sesión → no mostrar login/registro
+    }
+}
+
+
+    function cerrarSesion() {
+        localStorage.removeItem("usuario");
+        window.location.href = "login.html";
+    }
